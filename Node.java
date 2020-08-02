683c7d8c7b7f3fcb71eb997a37b8afe28cecaf0d
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2020-08-01 22:24:34 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Node {
+    public int[] myNode;
+    public int place;
+    Node next;
+    int data;
+
+
+    public Node(int value){
+        data = value;
+        next = null;
+    }
+
+    public Node(){}
+}
